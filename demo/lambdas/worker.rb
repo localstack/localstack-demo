5bf6a22124e52f5683e074aef1e7cc4f88e06789
Waldemar Hummer <waldemar.hummer@gmail.com>
2022-10-21 09:25:25 +0200

    refactor sample app to deploy and run against real AWS (#37)

--- demo/lambdas/worker.rb
+++ demo/lambdas/worker.rb
@@ -1,12 +1,21 @@
 require 'aws-sdk-states'
-ENV['AWS_SECRET_ACCESS_KEY'] = 'test'
-ENV['AWS_ACCESS_KEY_ID'] = 'test'
-$endpoint = "http://#{ENV['LOCALSTACK_HOSTNAME']}:4566"
-$state_machine_arn = 'arn:aws:states:us-east-1:000000000000:stateMachine:processingStateMachine'
+
+$localstack_hostname = ENV['LOCALSTACK_HOSTNAME']
+$endpoint = "http://#{$localstack_hostname}:4566"
+if $localstack_hostname
+    ENV['AWS_SECRET_ACCESS_KEY'] = 'test'
+    ENV['AWS_ACCESS_KEY_ID'] = 'test'
+end
+$state_machine_arn = ENV['STATE_MACHINE_ARN']
 
 def triggerProcessing(event:, context:)
+    if $localstack_hostname
+        client = Aws::States::Client.new(:endpoint => $endpoint)
+    else
+        client = Aws::States::Client.new()
+    end
+
     records = event['Records']
-    client = Aws::States::Client.new(:endpoint => $endpoint)
     for rec in records do
         result = client.start_execution({
             state_machine_arn: $state_machine_arn,
